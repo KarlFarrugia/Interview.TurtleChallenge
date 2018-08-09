ab8f8afa0c2fbd8e8074efa2d079a6329061e1ea
Karl Farrugia <karlfarrugia@live.com>
2018-08-09 14:17:35 +0200

    added a unit test

--- UnitTests/Tests.cs
+++ UnitTests/Tests.cs
@@ -304,5 +304,23 @@ namespace UnitTests
                 ElementAt(0).Split(' ').Select(s => Enum.Parse(typeof(Moves), s)).Cast<Moves>().ToList();
             Assert.AreEqual("Still in danger!", ExecuteGame.Execute(testProgram.Board, testProgram.Turtle, moves));
         }
+        
+        /// <summary>
+        /// Tests that a turtle cannot move out of board.
+        /// </summary>    
+        [TestCase]
+        public void TestTurtleCannotMoveOutOfBoard()
+        {
+            var settings = new string[6];
+            settings[0] = "5 4";
+            settings[1] = "1,1 1,3 3,3";
+            settings[2] = "4 2";
+            settings[3] = "0 1 N";
+            settings[4] = "M M M M M";
+            var testProgram = new ConfigurationsReader(settings);
+            IEnumerable<Moves> moves = testProgram.Games.
+                ElementAt(0).Split(' ').Select(s => Enum.Parse(typeof(Moves), s)).Cast<Moves>().ToList();
+            Assert.Throws<Exception>(() => ExecuteGame.Execute(testProgram.Board, testProgram.Turtle, moves));
+        }
     }
 }
